c32f54c613fffcbc0a8173907f9be200c57f02a6
skarbazdev <shaikhdevops101@gmail.com>
2023-02-21 20:21:38 +0530

    Register model

--- home/admin.py
+++ home/admin.py
@@ -2,10 +2,4 @@ from django.contrib import admin
 from . models import Todo
 # Register your models here.
 
-# admin.site.register(Todo)
-
-@admin.register(Todo)
-class Todo(admin.ModelAdmin):
-    list_display = ['title','complete','pub_date']
-    list_filter = ['complete', 'pub_date']
-    readonly_fields = ['pub_date']
+admin.site.register(Todo)
